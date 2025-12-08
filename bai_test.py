da1a612b8afbea54f4de86cc63cdc8fb1abd8df4
bientrung12 <trung.bt.67khmt@ntu.edu.vn>
2025-12-08 14:03:02 +0700

    Update bai_test.py

--- bai_test.py
+++ bai_test.py
@@ -2,16 +2,15 @@
 a=int(input("Nhập số a: "))
 b=int(input("Nhập số b: "))
 c=int(input("Nhập số c: "))
-
 print("Tổng=", a+b+c)
 
 #bai 2 viets pt nhập vào số nguyên cho biết số đố có đồng thời chia hết cho 3 và 5 hay không
-number_str = input("Nhập một số nguyên: ")
-number = int(number_str)
-if number % 3 == 0 and number % 5 ==0:
-    print(f"{number} là số chia hết cho 3 và 5.")
+n = int(input("Nhập một số nguyên: "))
+
+if n % 3 == 0 and n % 5 == 0:
+    print(f"{n} là số chia hết cho 3 và 5.")
 else:
-  print(f"{number} là số không chia hết cho 3 và 5.")
+  print(f"{n} là số không chia hết cho 3 và 5.")
 
 
   
