2a208b7bee77c9afc963934b448fde5ec5743ab5
bientrung12 <trung.bt.67khmt@ntu.edu.vn>
2025-12-29 18:32:34 +0700

    Create Kiem_tra_cau1B.py

---
+++ Kiem_tra_cau1B.py
@@ -0,0 +1,10 @@
+a=int(input("Nhập số a: "))
+b=int(input("Nhập số b: "))
+if a==0 :
+ if b==0 :
+    print("Phương trình có vô số nghiệm")
+ else:
+    print("Phương trình vô nghiệm")
+else:
+    print("Phương trình có 1 nghiệm x= ",-b/a)
+      
